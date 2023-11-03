84ec0b55a0dc4affd2dd5d5f9a233f65c40bd634
Andrew Payne <andrewpayne64@gmail.com>
2023-11-03 13:52:48 -0400

    Add styles

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,20 +1,27 @@
+import { NotificationIcon } from "../icons/NotificationIcon";
+
 const Topbar: React.FC = () => {
   return (
-    <div className="p-3 flex justify-between items-center text-black rounded-r-2xl border border-gray-300">
+    <div className="p-3 flex justify-between items-center text-black rounded-2xl border border-gray-300">
       <span className="mr-2 font-bold text-2xl">Workout</span>
       <div className="relative">
         <input
           type="text"
-          placeholder="Search..."
+          placeholder="Search here..."
           className="bg-white bg-opacity-25 px-3 py-2 rounded-full text-sm outline-none"
         />
         {/* <SearchIcon className="h-6 w-6 absolute top-2 right-3 text-white" /> */}
       </div>
-      <div className="flex">
-        {/* <BellIcon className="h-6 w-6 mx-2 cursor-pointer" /> */}
-        <div className="flex items-center cursor-pointer">
-          <span className="mr-2">My Account</span>
-          {/* <ChevronDownIcon className="h-5 w-5" /> */}
+      <div className="flex gap-6 items-center">
+        <NotificationIcon />
+        <img
+          src="/images/user-profile.png"
+          alt=""
+          className="h-8 w-8 rounded"
+        />
+        <div className="flex flex-col items-center cursor-pointer">
+          <span className="text-sm">Jane Doerr</span>
+          <span className="text-xs font-light">jdoerr@gmail.com</span>
         </div>
       </div>
     </div>
